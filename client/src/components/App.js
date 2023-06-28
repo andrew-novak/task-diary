eac3567c6372e2386bd31e1b7f47f11e5ac29414
andrew-novak <contact@andrewnovak.co.uk>
2023-06-28 22:21:13 +0100

    added: basename prop to <Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Provider as StoreProvider } from 'react-redux';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import { CssBaseline } from '@material-ui/core';
-import { Switch, Router, Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Provider as StoreProvider } from "react-redux";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import { CssBaseline } from "@material-ui/core";
+import { Switch, Router, Route, Redirect } from "react-router-dom";
 
-import store from '../store';
-import history from '../history';
-import PrivateRoute from './PrivateRoute';
-import { MainPage } from './private';
-import { ErrorAuthPage, ErrorGeneralPage } from './errors';
-import { LoginPage, RegisterPage } from './auth';
+import store from "../store";
+import history from "../history";
+import PrivateRoute from "./PrivateRoute";
+import { MainPage } from "./private";
+import { ErrorAuthPage, ErrorGeneralPage } from "./errors";
+import { LoginPage, RegisterPage } from "./auth";
 
 const theme = createMuiTheme({
   spacing: 8,
@@ -27,22 +27,22 @@ const theme = createMuiTheme({
   loadingHeight: 6,
   palette: {
     primary: {
-      main: '#0748db',
+      main: "#0748db",
       //main: deepPurple['A700'],
-      contrastText: 'rgb(255,255,255)',
+      contrastText: "rgb(255,255,255)",
     },
     error: {
-      main: '#e90e0a',
-      background: '#ffeeee',
+      main: "#e90e0a",
+      background: "#ffeeee",
       //main: red[700],
       //background: red[50],
     },
     label: {
-      main: '#616161',
+      main: "#616161",
       //main: grey[700],
     },
     outline: {
-      main: '#9e9e9e',
+      main: "#9e9e9e",
       //main: grey[500],
     },
   },
@@ -65,19 +65,21 @@ function App() {
   });
   */
 
+  const basename = process.env.PUBLIC_URL;
+
   return (
-    <StoreProvider store={ store }>
-      <ThemeProvider theme={ theme }>
-        <MuiPickersUtilsProvider utils={ DateFnsUtils }>
+    <StoreProvider store={store}>
+      <ThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <CssBaseline />
-          <Router history={ history }>
+          <Router history={history} basename={basename}>
             <Switch>
-              <PrivateRoute exact path='/' component={ MainPage } />
-              <Route exact path='/error-auth' component={ ErrorAuthPage } />
-              <Route exact path='/error-general' component={ ErrorGeneralPage } />
-              <Route exact path='/login' component={ LoginPage } />
-              <Route exact path='/register' component={ RegisterPage } />
-              <Redirect from='*' to='/' />
+              <PrivateRoute exact path="/" component={MainPage} />
+              <Route exact path="/error-auth" component={ErrorAuthPage} />
+              <Route exact path="/error-general" component={ErrorGeneralPage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/register" component={RegisterPage} />
+              <Redirect from="*" to="/" />
             </Switch>
           </Router>
         </MuiPickersUtilsProvider>
