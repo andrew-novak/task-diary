03529773288876d20b98f8a35feffaf204970efa
andrew-novak <contact@andrewnovak.co.uk>
2023-06-30 04:56:33 +0100

    dont redux-logger log in production

--- client/src/store.js
+++ client/src/store.js
@@ -1,21 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import rootReducer from './reducers'
+import rootReducer from "./reducers";
+
+const NODE_ENV = process.env.NODE_ENV;
 
 const initialState = {};
 
 const logger = createLogger({});
 
 // Note: logger must be the last middleware in chain, otherwise it will log thunk and promise, not actual actions.
-const middleware = [thunk, logger];
+const middleware = [thunk, ...(NODE_ENV !== "production" ? { logger } : {})];
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware)),
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
