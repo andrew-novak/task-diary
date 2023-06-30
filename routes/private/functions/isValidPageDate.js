331a7753688bf856a097e70a0b1ffd568ee965be
andrew-novak <contact@andrewnovak.co.uk>
2023-06-30 03:37:13 +0100

    changes: more  debug console.logs added to isValidPageDate

--- routes/private/functions/isValidPageDate.js
+++ routes/private/functions/isValidPageDate.js
@@ -3,6 +3,10 @@ const isValidDate = require("./isValidDate");
 function isValidPageDate(date) {
   console.log("date:", date);
   console.log("isValidDate(date):", isValidDate(date));
+  console.log("date.getHours() !== 0", date.getHours() !== 0);
+  console.log("ddate.getMinutes() !== 0", date.getMinutes() !== 0);
+  console.log("date.getSeconds() !== 0", date.getSeconds() !== 0);
+  console.log("date.getMilliseconds() !== 0", date.getMilliseconds() !== 0);
   if (!isValidDate(date)) return false;
   if (date.getHours() !== 0) return false;
   if (date.getMinutes() !== 0) return false;
