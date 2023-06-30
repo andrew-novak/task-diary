ffbc74eb702ebef43be21a0c7d0291cdf2e9cf0a
andrew-novak <contact@andrewnovak.co.uk>
2023-06-30 04:32:41 +0100

    BASE_URL added to logout link

--- client/src/components/private/MainPage/Body/components/Settings/AccountDetails.js
+++ client/src/components/private/MainPage/Body/components/Settings/AccountDetails.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+import { BASE_URL } from "../../../../../../constants/urls";
+
+const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: -theme.margin.smItem,
     marginBottom: -theme.margin.smItem,
   },
   link: {
-    width: '100%',
-    textAlign: 'center',
+    width: "100%",
+    textAlign: "center",
     paddingTop: theme.margin.smItem,
     paddingBottom: theme.margin.smItem,
   },
@@ -18,9 +20,13 @@ const useStyles = makeStyles(theme => ({
 function AccountDetails(props) {
   const classes = useStyles();
   return (
-    <div className={ classes.root }>
-      <div className={ classes.link }>
-        <Link href='/login' variant='body1' className={ classes.lin }>
+    <div className={classes.root}>
+      <div className={classes.link}>
+        <Link
+          href={`${BASE_URL}/login`}
+          variant="body1"
+          className={classes.lin}
+        >
           Log out
         </Link>
       </div>
